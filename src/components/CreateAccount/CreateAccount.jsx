d5403896acb89efbb19ed3a3207d4da0237d474a
Sheena Abraham <SheenaPAbraham@gmail.com>
2022-10-31 16:22:51 -0400

    added useStates

---
+++ src/components/CreateAccount/CreateAccount.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+
+const CreateAccount = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    // States for checking the errors
+  const [submitted, setSubmitted] = useState(false);
+  const [error, setError] = useState(false);
+ 
+  // Handling the name change
+  const handleName = (e) => {
+    setName(e.target.value);
+    setSubmitted(false);
+  };
+ 
+  // Handling the email change
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+    setSubmitted(false);
+  };
+ 
+  // Handling the password change
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+    setSubmitted(false);
+  };
+  )
+}
+
+export default CreateAccount
