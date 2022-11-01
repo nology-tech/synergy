075bf5939ceea532a203713db977ce3bff16a23d
RFowlerDB <rachie.fowler@db.com>
2022-11-01 17:01:23 -0400

    contact list updated

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const ContactList = (props) => {
+    const {contactsArray} = props
+    const contactJSX = contactsArray.map (contact >= 
+        (
+      <div>
+        <div>bankIcon
+        <img
+                className="contact-bankIcon__img"
+                src={contact.bankIcon}
+                alt={contact.name}
+                            />
+        </div>
+        <div>{contact.name}        </div>
+        <div>{contact.sortCode}</div>
+        <div>{contact.Account</div>
+        <div>{contact.bankName}</div>
+        <div>{contact.iban}</div>
+        <div>DeleteImage</div>
+      </div>
+  
+
+    return (
+    <div>ContactList</div>
+  )
+}
+
+export default ContactList
--- src/containers/ContactContainer/ContactContainer.jsx
+++ src/containers/ContactContainer/ContactContainer.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ContactSearch from '../../components/ContactSearch/ContactSearch'
 import "./ContactContainer.scss"
+import  Contacts from "../../assets/data/Contacts" 
+import ContactList from '../../components/ContactList/ContactList'
 
 const ContactContainer = () => {
   return ( 
@@ -9,6 +11,8 @@ const ContactContainer = () => {
       <div className='contact__Title'>Contacts</div>
       <h2 className='contact__Heading'>Contact List</h2>
       <ContactSearch />
+      <ContactList />
+      
       </div>
     </>
   )
