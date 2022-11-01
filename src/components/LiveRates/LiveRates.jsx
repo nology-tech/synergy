1c8004a97c97fa42cc6c36faf5b58e6d533370ea
Rupali Anjaria-Rami <rupalianjaria-rami@Rupalis-iMac.home>
2022-11-01 15:43:14 -0400

    Committing changes again

--- src/components/LiveRates/LiveRates.jsx
+++ src/components/LiveRates/LiveRates.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
+import currency from '../../data/currency.js'
+import LiveRatesList from '../LiveRatesList/LiveRatesList.jsx';
+// import LiveRatesList from '../LiveRatesList/LiveRatesList'
+
+
 
 const LiveRates = () => {
+    console.log(currency[1].name);
   return (
-    <div>
-        <h4>Transfer</h4>
-        <h2>View Latest Rates</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, voluptatem quidem? Corporis, odio vero sequi vitae deleniti sit quod quae! Autem unde reiciendis sunt porro obcaecati alias fugiat quod aliquid!
+    <div className='live-rates'>
+        <h4 className='live-rates__h4'>Transfer</h4>
+        <h2 className='live-rates__h2'>View Latest Rates</h2>
+        <p className='live-rates__para'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, voluptatem quidem? Corporis, odio vero sequi vitae deleniti sit quod quae! Autem unde reiciendis sunt porro obcaecati alias fugiat quod aliquid!
         </p>
+        <LiveRatesList className="live-rates__list" currency={currency} />
     </div>
+
   )
 }
 
---
+++ src/components/LiveRatesList/LiveRatesList.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./LiveRatesList.scss";
+
+const LiveRatesList = (props) => {
+  const { currency } = props;
+  const currencyJSX = currency.map((currency, index) => (
+    <div className="currency-row currency-list__items" key={currency.id}>
+      {/* <img className= "currency-list__img" src={currency.img} alt="" /> */}
+      <div className="currency-list__name">{currency.name}</div>
+      <div className="currency-list__amount">{currency.amount}</div>
+      <div className="currency-list__rate">{currency.rate}</div>
+    </div>
+  ));
+  return (
+    <>
+      <div className="currency-row currency-list__heading">
+        <div className="currency-list__name">Currency</div>
+        <div className="currency-list__amount">Amount</div>
+        <div className="currency-list__rate">Rate</div>
+      </div>
+      <div>{currencyJSX}</div>
+    </>
+  )
+};
+
+export default LiveRatesList;
