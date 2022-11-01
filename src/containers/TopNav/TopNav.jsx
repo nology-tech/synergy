724d056e5bd1c087f7570061cdc540925a839793
ppatilgit <pavitra.raleigh@gmail.com>
2022-11-01 16:09:54 -0400

    Top Nav styling

--- src/containers/TopNav/TopNav.jsx
+++ src/containers/TopNav/TopNav.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import React from "react";
 import "./TopNav.scss";
 import NavMenu from "../../components/NavMenu/NavMenu";
+import logo from "../../assets/images/Main_Logo.png"
 
 const TopNav = () => {
   const [showNav, setShowNav] = useState(true);
@@ -10,22 +11,24 @@ const TopNav = () => {
     setShowNav(!showNav);
   };
 
+  let mainLogo= logo;
+
   return (
     <div className="nav">
       <div className="topnav">
         <div className="topnav__menu"> 
-          <img className="topnav__appicon" src="" alt="Synergy-icon" onClick={toggleNav} />
-          <a href="#" onClick={toggleNav}>Synergy</a>
+          <img className="topnav__appicon" src={mainLogo} alt="Synergy-icon" onClick={toggleNav} />
+  
         </div>
         <div className="topnav__centerlinks">
-          <p>Home</p>
-          <p>Features</p>
-          <p>About</p>
-          <p>Contact Us</p>
+          <a href="#" className="topnav__link navHome">Home</a>
+          <a href="#" className="topnav__link navFeatures">Features</a>
+          <a href="#" className="topnav__link navAbout">About</a>
+          <a href="#" className="topnav__link navContact">Contact Us</a>
         </div>
         <div className="topnav__signin">
-          <p>Sign In</p>
-          <p>SignUp</p>
+          <a href ="#" className="signin">Sign In</a>
+          <a href ="#" className="signup">SignUp</a>
         </div>
       </div>
       {showNav && <NavMenu title="Synergy"/>}
