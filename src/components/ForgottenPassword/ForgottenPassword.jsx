0ab684e7038eebe5af4b6e383300ffca248e545c
shushpan-v-satori <ianastasieva@gmail.com>
2022-11-02 10:33:34 -0400

    Moved styles one level up

--- src/components/ForgottenPassword/ForgottenPassword.jsx
+++ src/components/ForgottenPassword/ForgottenPassword.jsx
@@ -1,35 +1,40 @@
-import React from 'react'
-import "./ForgottenPassword.scss"
-import synergy from "../../assets/images/synergy.png"
-
+import React from "react";
+import "./ForgottenPassword.scss";
+import synergy from "../../assets/images/synergy.png";
 
 const ForgottenPassword = () => {
   return (
     <>
-        <nav>Navbar placeholder</nav>
-        <main className="backgrounds">
+      <nav>Navbar placeholder</nav>
+      <main className="background">
         <div className="loginFlow">
-            <div className="loginFlow__synergyIcon">
-                <img src={synergy} alt="Synergy icon" className="loginFlow__synergyImg"/>
-                <h1 >Synergy</h1>    
+          <div className="loginFlow__synergyIcon">
+            <img
+              src={synergy}
+              alt="Synergy icon"
+              className="loginFlow__synergyImg"
+            />
+            <h1>Synergy</h1>
+          </div>
+          <div className="loginFlow__main">
+            <div className="loginFlow__main__header">
+              <h1>Forgotten your password</h1>
             </div>
-            <div className="loginFlow__main">
-                <div className="loginFlow__main__header">
-                <h1>Forgotten your password</h1>
-                <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-            </div>
-            <div className="loginFlow__credentials"></div>   
-                 <h2 className="loginFlow__credentials__header">Email</h2>
-                <input className="loginFlow__inputbox" type="text" placeholder="enter your email"/> 
-            </div>
-            <div>
-                    <button>Send</button>
-            </div>    
 
-        </div>    
-        </main>
+            <div className="loginFlow__credentials">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+              <h2 className="loginFlow__credentials__header">Email</h2>
+              <input className="loginFlow__inputbox" type="text" />
+            </div>
+
+            <div className="loginFlow__button">
+              <button>Send</button>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
-  )
-}
+  );
+};
 
-export default ForgottenPassword
+export default ForgottenPassword;
--- src/components/LoginFlow/LoginFlow.jsx
+++ src/components/LoginFlow/LoginFlow.jsx
@@ -1,45 +1,64 @@
-import React from 'react'
-import "./LoginFlow.scss"
-import emojihand from "../../assets/images/Emojihand.png"
-import synergy from "../../assets/images/synergy.png"
+import React from "react";
+import "./LoginFlow.scss";
+import emojihand from "../../assets/images/Emojihand.png";
+import synergy from "../../assets/images/synergy.png";
 
 const LoginFlow = () => {
   return (
-       <>
-        <nav>Navbar placeholder</nav>
-        <main className="background">
-             <div className="loginFlow">
-            <div className="loginFlow__synergyIcon">
-                <img src={synergy} alt="Synergy icon" className="loginFlow__synergyImg"/>
-                <h1 >Synergy</h1>    
+    <>
+      <nav>Navbar placeholder</nav>
+      <main className="background">
+        <div className="loginFlow">
+          <div className="loginFlow__synergyIcon">
+            <img
+              src={synergy}
+              alt="Synergy icon"
+              className="loginFlow__synergyImg"
+            />
+            <h1>Synergy</h1>
+          </div>
+          <div className="loginFlow__main">
+            <div className="loginFlow__main__header">
+              <h1>Welcome Back!</h1>
+              <img
+                src={emojihand}
+                alt="Handwave"
+                className="loginFlow__main__img"
+              />
             </div>
-            <div className="loginFlow__main">
-                <div className="loginFlow__main__header">
-                <h1>Welcome Back!</h1>
-                <img src={emojihand} alt="Handwave" className="loginFlow__main__img"/>
-                </div>
 
-                <div className="loginFlow__credentials">
-                <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu leo urna.</p>
-                <h2 className="loginFlow__credentials__header">Email</h2>
-                <input className="loginFlow__inputbox" type="text" />
-                <h2 className="loginFlow__credentials__header">Password</h2>
-                <input className="loginFlow__inputbox" type="text" />
-                </div>
-
-                <div className="loginFlow__links">
-                    <p className="grey">Don't have an account? <a href="#sugnup" className="blue">Sign Up</a></p>
-                    <p><a href="#forgottenpsw" className="blue">Forgotten Password?</a></p>
-                </div>
-                <div className="loginFlow__button">
-                    <button>Login</button>
-                </div>
+            <div className="loginFlow__credentials">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu
+                leo urna.
+              </p>
+              <h2 className="loginFlow__credentials__header">Email</h2>
+              <input className="loginFlow__inputbox" type="text" />
+              <h2 className="loginFlow__credentials__header">Password</h2>
+              <input className="loginFlow__inputbox" type="text" />
             </div>
+
+            <div className="loginFlow__links">
+              <p className="grey">
+                Don't have an account?{" "}
+                <a href="#sugnup" className="blue">
+                  Sign Up
+                </a>
+              </p>
+              <p>
+                <a href="#forgottenpsw" className="blue">
+                  Forgotten Password?
+                </a>
+              </p>
+            </div>
+            <div className="loginFlow__button">
+              <button>Login</button>
+            </div>
+          </div>
         </div>
-        </main>
-        </> 
-    
-  )
-}
+      </main>
+    </>
+  );
+};
 
-export default LoginFlow
+export default LoginFlow;
