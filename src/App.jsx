8a6f27c512d29119560c8911da47b37498050250
Rupali Anjaria-Rami <rupalianjaria-rami@Rupalis-iMac.home>
2022-11-01 20:57:09 -0400

    Added sidebar and navbar images

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import "./App.scss";
 import LiveRates from "./components/LiveRates/LiveRates";
+import sidebar from "./assets/images/sidebar3.jpg";
+import navbar from "./assets/images/navbar.png";
 
 const App = () => {
   return (
-    <div>
-      <h1>Synergy</h1>
-      <LiveRates />
+    <div className="liveRates">
+      {/* <h1>Synergy</h1> */}
+      <img 
+        className="liveRates__sidebar" 
+        src={sidebar} 
+        alt="Synergy" 
+      />
+    
 
+      <div className="liveRates__section">
+        <img 
+          className="liveRates__navbar" 
+          src={navbar} 
+          alt="" 
+        />
+        <LiveRates className="liveRates__main" />
+      </div>
     </div>
   )
 }
