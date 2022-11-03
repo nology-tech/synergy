fc317e827772f475bca8ae61363fdf5e7ce87480
RFowlerDB <rachie.fowler@db.com>
2022-11-03 17:01:28 -0400

    changes for Thursday

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.scss";
 import ContactContainer from "./containers/ContactContainer/ContactContainer";
-
+import NavBar from "../src/assets/images/navbar.png";
+import SideBar from "../src/assets/images/sidebar.jpg";
 
 const App = () => {
   return (
     <>
+    <div className="placeHolder_Navbar">
+      <img src={NavBar} alt="NavBar" />
+    </div>
+    
     <div className="App">
+    <div className="placeHolder_Sidebar"><img src={SideBar} alt="SideBar" /></div>
       <ContactContainer />
     </div>
        
