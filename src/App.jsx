31d096d4f8482cb927ddc6ea6e9f5b2e1e8cfbbb
Tet <Maria-Theresa.Edralin@db.com>
2022-11-04 10:04:01 -0400

    tried to do routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,32 @@ import "./App.scss";
 import ContactContainer from "./containers/ContactContainer/ContactContainer";
 import NavBar from "../src/assets/images/navbar.png";
 import SideBar from "../src/assets/images/sidebar.jpg";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
 
 const App = () => {
   return (
-    <div className="App">
-      <div className="placeHolder_Sidebar">
-        <img src={SideBar} alt="SideBar" />
-      </div>
-      <div className="right-side">
-        <div className="placeHolder_Navbar">
-          <img src={NavBar} alt="NavBar" />
+    <Router>
+      <div className="App">
+        <div className="placeHolder_Sidebar">
+          <img src={SideBar} alt="SideBar" />
         </div>
-        <ContactContainer />
+        <div className="right-side">
+          <div className="placeHolder_Navbar">
+            <img src={NavBar} alt="NavBar" />
+          </div>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <ContactContainer />
+              }>
+            </Route>
+          </Routes>
+        </div>
       </div>
-    </div>
+
+    </Router>
   );
 };
 
