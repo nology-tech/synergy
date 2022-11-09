44b00cdbcbff5c9ac209b06740b375b450cfb9de
Leah Oshry <owlinnyc@gmail.com>
2022-11-09 16:29:48 -0500

    updated currency.js and added Currency Converter pages

--- src/data/currency.js
+++ src/data/currency.js
@@ -27,41 +27,41 @@ const currency = [
         "amount": "82.7545",
         "rate": "0.0120"
     },
-    // {
-    //     "id": 5,
-    //     "img": "",
-    //     "name": "Australian Dollar",
-    //     "amount": "1.563354",
-    //     "rate": "0.639650"
-    // },
-    // {    
-    //     "id": 6,
-    //     "img": "",
-    //     "name": "Canadian Dollar",
-    //     "amount": "1.362231",
-    //     "rate": "0.734090"
-    // },
-    // {
-    //     "id": 7,
-    //     "img": "",
-    //     "name": "Singapore Dollar",
-    //     "amount": "1.415484",
-    //     "rate": "0.706472"
-    // },
-    // {    
-    //     "id": 8,
-    //     "img": "",
-    //     "name": "Swiss Franc",
-    //     "amount": "1.001610",
-    //     "rate": "0.998393"
-    // },
-    // {    
-    //     "id": 9,
-    //     "img": "",
-    //     "name": "Malaysian Ringgit",
-    //     "amount": "4.723077",
-    //     "rate": "0.211726"
-    // },
+    {
+        "id": 5,
+        "img": "https://flagcdn.com/16x12/au.png",
+        "name": "Australian Dollar",
+        "amount": "1.563354",
+        "rate": "0.639650"
+    },
+    {    
+        "id": 6,
+        "img": "https://flagcdn.com/16x12/ca.png",
+        "name": "Canadian Dollar",
+        "amount": "1.362231",
+        "rate": "0.734090"
+    },
+    {
+        "id": 7,
+        "img": "https://flagcdn.com/16x12/sg.png",
+        "name": "Singapore Dollar",
+        "amount": "1.415484",
+        "rate": "0.706472"
+    },
+    {    
+        "id": 8,
+        "img": "https://flagcdn.com/16x12/ch.png",
+        "name": "Swiss Franc",
+        "amount": "1.001610",
+        "rate": "0.998393"
+    },
+    {    
+        "id": 9,
+        "img": "https://flagcdn.com/16x12/my.png",
+        "name": "Malaysian Ringgit",
+        "amount": "4.723077",
+        "rate": "0.211726"
+    },
     {
         "id": 10,
         "img": "https://flagcdn.com/16x12/jp.png",
@@ -69,49 +69,15 @@ const currency = [
         "amount": "148.6933",
         "rate": "0.0067"
     },
-    // {    
-    //     "id": 11,
-    //     "img": "",
-    //     "name": "Chinese Yuan Renminbi",
-    //     "amount": "7.302940",
-    //     "rate": "0.136931"
-    // }
+    {    
+        "id": 11,
+        "img": "https://flagcdn.com/16x12/cn.png",
+        "name": "Chinese Yuan Renminbi",
+        "amount": "7.302940",
+        "rate": "0.136931"
+    }
 ]
 
-// {
-//     "id": 0,
-//     "img": "",
-//     "name": "Currency",
-//     "amount": "Amount",
-//     "rate": "Rate"
-// },
-// {
-//     "id": 1,
-//     "img": "https://flagicons.lipis.dev/flags/4x3/us.svg",
-//     "name": "US Dollar",
-//     "amount": "1.00",
-//     "rate": ""
-// },
-// {    
-//     "id": 2,
-//     "img": "https://flagicons.lipis.dev/flags/4x3/eu.svg",
-//     "name": "Euro",
-//     "amount": "1.011850",
-//     "rate": "0.988288"
-// },
-// {
-//     "id": 3,
-//     "img": "https://flagicons.lipis.dev/flags/4x3/gb.svg",
-//     "name": "British Pound",
-//     "amount": "0.872085",
-//     "rate": "1.146677"
-// },
-// {    
-//     "id": 4,
-//     "img": "https://flagicons.lipis.dev/flags/4x3/in.svg",
-//     "name": "Indian Rupee",
-//     "amount": "82.754566",
-//     "rate": "0.012084"
-// }
+
 
 export default currency;
