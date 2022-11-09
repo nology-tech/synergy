78ebe5c94813e0d3d452beb8a3448138e4fceff6
Leah Oshry <owlinnyc@gmail.com>
2022-11-08 19:38:40 -0500

    Merge feature/liveRates into master

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,124 @@
-import "./App.scss";
+import { useState } from "react";
 import React from "react";
-import LiveRates from "./components/LiveRates/LiveRates";
-import sidebar from "./assets/images/sidebar3.jpg";
-import navbar from "./assets/images/topnav.png";
+
+import "./App.scss";
+import Main from "./containers/Main/Main";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignIn from "./containers/SignIn/SignIn";
+import SignUp from "./containers/SignUp/SignUp";
+import UserDashboard from "./containers/UserDashboard/UserDashboard";
+import ForgottenPassword from "./components/ForgottenPassword/ForgottenPassword";
+import LoginFlowChangePsw from "./components/LoginFlowChangePsw/LoginFlowChangePsw";
+import LoginFlowWelcome from "./components/LoginFlowWelcome/LoginFlowWelcome";
+import SignUp from "./components/SignUp/SignUp";
+import BankDetails from "./components/BankDetails/BankDetails";
+import BillingAddress from "./components/BillingAddress/BillingAddress";
+import LandingMain from "./containers/LandingMain/LandingMain";
+import ContactListPage from "./components/ContactListPage/ContactListPage";
+import LiveRates from "./components/LiveRates/LiveRates";
 
 const App = () => {
-  return ( 
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleInput = (event) => {
+    const cleanInput = event.target.value.toLowerCase();
+    setSearchTerm(cleanInput);
+  };
+
+  return (
     <Router>
-    
-    <div className="app">
-      {/* <h1>Synergy</h1> */}
-      <img  
-        src={sidebar} 
-        alt="Synergy" 
-      />
-    
-
-      <div>
-        <img 
-          src={navbar} 
-          alt="" 
-        />
+      <div className="app">
         <Routes>
-        <Route path="/liverates" element={<LiveRates />} />
-
-        {/* <Route path="/albums/:albumId" element={<AlbumInfo albumsArr={filteredAlbums} />} /> */}
+          <Route path="/" element={<LandingMain />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/bankdetails" element={<BankDetails />} />
+          <Route path="/billingaddress" element={<BillingAddress />} />
+          <Route path="/welcome" element={<LoginFlowWelcome />} />
+          <Route path="/forgotten-password" element={<ForgottenPassword />} />
+          <Route path="/change-password" element={<LoginFlowChangePsw />} />
+          <Route path="/contacts" element={<ContactListPage />} />
+          <Route path="/liverates" element={<LiveRates />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />}></Route>
+          <Route
+            path="/userprofile"
+            element={
+              <UserDashboard
+                handleInput={handleInput}
+                value={searchTerm}
+                navigateTo="Wallet"
+              />
+            }
+          ></Route>
+          <Route
+            path="/dashboard"
+            element={
+              <UserDashboard
+                handleInput={handleInput}
+                value={searchTerm}
+                navigateTo="Wallet"
+              />
+            }
+          ></Route>
+          <Route
+            path="/dashboard/wallet"
+            element={
+              <UserDashboard
+                handleInput={handleInput}
+                value={searchTerm}
+                navigateTo="Wallet"
+              />
+            }
+          ></Route>
+          <Route
+            path="/dashboard/liveRates"
+            element={
+              <UserDashboard
+                handleInput={handleInput}
+                value={searchTerm}
+                navigateTo="LiveRates"
+              />
+            }
+          ></Route>
+          <Route
+            path="/dashboard/convert"
+            element={
+              <UserDashboard
+                handleInput={handleInput}
+                value={searchTerm}
+                navigateTo="Convert"
+              />
+            }
+          ></Route>
+          <Route
+            path="/dashboard/transfer"
+            element={
+              <UserDashboard
+                handleInput={handleInput}
+                value={searchTerm}
+                navigateTo="Transfer"
+              />
+            }
+          ></Route>
+          <Route
+            path="/dashboard/contacts"
+            element={
+              <UserDashboard
+                handleInput={handleInput}
+                value={searchTerm}
+                navigateTo="Contacts"
+              />
+            }
+          ></Route>
+          <Route path="/home" element={<Main />}></Route>
+          <Route path="/features" element={<Main />}></Route>
+          <Route path="/about" element={<Main />}></Route>
+          <Route path="/contact" element={<Main />}></Route>
+          <Route path="/" element={<Main />}></Route>
         </Routes>
       </div>
-     
-    </div>
     </Router>
-  )
-}
+  );
+};
 
 export default App
