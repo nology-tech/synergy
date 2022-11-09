7bb1365ac73ba46e12bed63a42586a38ea9e66f0
RFowlerDB <rachie.fowler@db.com>
2022-11-09 16:48:45 -0500

    Dummy UserInfo created

---
+++ src/Assets/data/UserInfo.js
@@ -0,0 +1,39 @@
+const userInfo = [
+    {
+      firstName: "Samantha",
+      lastName: "Brooks",
+      accountBalance:"15,210", 
+      loginEmail: "S.brooks@synergy.com"
+    },
+
+    {
+        firstName: "John",
+        lastName: "Brooks",
+        accountBalance: "8,210",
+        loginEmail: "J.brooks@synergy.com" 
+      },
+      {
+        firstName: "James",
+        lastName: "Brooks",
+        accountBalance: "6,210",
+        loginEmail: "James.brooks@synergy.com" 
+      },
+      {
+        firstName: "Cynthia",
+        lastName: "Brooks",
+        accountBalance: "2,210",
+        loginEmail: "C.brooks@synergy.com" 
+      },
+      {
+        firstName: "Tasha",
+        lastName: "Brooks",
+        accountBalance: "5,210",
+        loginEmail: "T.brooks@synergy.com" 
+      },
+      {
+        firstName: "Elizabeth",
+        lastName: "Brooks",
+        accountBalance: "16,512",
+        loginEmail: "E.brooks@synergy.com" 
+      },
+    ]
