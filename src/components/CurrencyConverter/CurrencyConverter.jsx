e2e2fd29591425fe5478477c714deff96a86e82a
Leah Oshry <owlinnyc@gmail.com>
2022-11-10 16:46:09 -0500

    added styling

--- src/components/CurrencyConverter/CurrencyConverter.jsx
+++ src/components/CurrencyConverter/CurrencyConverter.jsx
@@ -15,7 +15,8 @@ const CurrencyConverter = () => {
     const fxRate = currency[1].rate;
     const currencyFromName = currency[0].name;
     const currencyToName = currency[1].name;
-    const message = `${amount} ${currencyFromName} = ${convertedAmount} ${currencyToName}`
+    const messageFrom = `${amount} ${currencyFromName} =`
+    const messageTo =  `${convertedAmount} ${currencyToName}`
 
     const flip = () => {
       var temp = from;
@@ -39,31 +40,31 @@ const CurrencyConverter = () => {
       };
   return (
     <div className='currencyConverter'>
-       <h4>Convert</h4>
-       <h2>Currency Converter</h2>  
-       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea dolorem nihil neque laudantium assumenda totam amet alias, sequi fuga officiis asperiores beatae provident iste. Quis ducimus nobis a officia earum.</p>   
+       <h4 className="currencyConverter__title1">Convert</h4>
+       <h2 className="currencyConverter__title2">Currency Converter</h2>  
+       <p className="currencyConverter__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea dolorem nihil neque laudantium assumenda totam amet alias, sequi fuga officiis asperiores beatae provident iste. Quis ducimus nobis a officia earum.</p>   
        <div className='currencyConverter__main'>
           <div className='currencyConverter__main__walletTemp'>Wallet Placeholder</div>
           <div className='currencyConverter__main__converter'>Converter
-          <label>Amount</label>
+          <label className="currencyConverter__main__converter__label">Amount</label>
                 <input
                   onChange={handleAmount}
                   className="input"
                   value={amount}
                   type="text"
                 />
-                <label>From</label>
+                <label className="currencyConverter__main__converter__label">From</label>
                 {/* From: Base Currency in Wallet/Live Rates -- assuming Base Currency = USD */}
                 <input
                
                  value={from}
-                //   className="input"
+                className="input"
                 
                   type="text" 
-                  disabled
+                  
                 />
-                <button onClick={flip}>reverse</button>
-                <label>To</label>
+                <button  className="currencyConverter__main__converter__button" onClick={flip}>reverse</button>
+                <label className="currencyConverter__main__converter__label">To</label>
                 {/* To: Desired currency selected via Live Rates */}
                 <input
                 //   onChange={handleAccountName}
@@ -73,7 +74,8 @@ const CurrencyConverter = () => {
                 />
                 <button onClick={convertAmount}>Convert</button>
                 {/* will display conversion */}
-                <p>{message}</p>
+                <p className="currencyConverter__message-from">{messageFrom}</p>
+                <p className="currencyConverter__message-to">{messageTo}</p>
 
           </div>
        </div>
