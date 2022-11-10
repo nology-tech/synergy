398d7c5b8e9f1a5ecf42c5350aa09788c9862dcf
RFowlerDB <rachie.fowler@db.com>
2022-11-10 17:47:01 -0500

    style changes and etc

--- src/components/FundsBox/FundsBox.jsx
+++ src/components/FundsBox/FundsBox.jsx
@@ -1,15 +1,24 @@
 import React from "react";
 import "./FundsBox.scss";
+import BlueSquare from "../../assets/images/wallet_bluesquare.png"
+
 
 const FundsBox = (props) => {
   const { fundsAmount, forConverter, name } = props;
+
   return (
     <>
-      <div className="fundsbox__firstName">{"Hey " + name}</div>
+    <h2 className="Greeting__firstName">{"Hey " + name}</h2>
       <div className="fundsBox">
-        <h3>Your Fund</h3>
+        <div className="fundsBox__content">
+          <img className="fundsBox_square" src={BlueSquare} alt="BlueSquareBox"/>
+            <h3 className="fundsBox__content__title">Your Fund</h3>
+            <div className="fundsBox__content__amount">{fundsAmount}</div>
+            <button className="fundsBox__content__manage"> Manage </button>
+        </div>
+        
       </div>
-      <div className="fundsBox__amount">{fundsAmount}</div>
+      
     </>
   );
 };
--- src/containers/Wallet/Wallet.jsx
+++ src/containers/Wallet/Wallet.jsx
@@ -1,18 +1,28 @@
-import React from 'react'
-import FundsBox from '../../components/FundsBox/FundsBox'
-import userInfo from '../../data/userInfo.js'
+import React from 'react';
+import FundsBox from '../../components/FundsBox/FundsBox';
+import userInfo from '../../data/userInfo.js';
+import NavMenu from '../../components/NavMenu/NavMenu';
+import ContactContainer from '../ContactContainer/ContactContainer';
+import NavBar from "../../assets/images/navbar.png";
 
 const Wallet = () => {
   return (
-    <div>
-        <h1>Funds box will go here</h1>
-        {/* {console.log(userInfo[0].accountBalance)}; */}
-        
-        <FundsBox 
-        name = {userInfo[0].firstName}
-        fundsAmount = {userInfo[0].accountBalance} />
-       
+    <div className='walletPage'>
+      <div className='walletPage__placeHolder'>
+          <div className="walletPage__placeHolder__Navbar"> <img src={NavBar} alt="NavBar" /> </div>
+          <div className="walletPage__placeHolder__NavMenu"> <NavMenu /> </div>     
+      </div>    
+      <div className='walletPage__details'>
+          <h1 className='walletPage__details__title'>Funds box will go here</h1>
+          {/* {console.log(userInfo[0].accountBalance)}; */}
+          <FundsBox 
+          name = {userInfo[0].firstName}
+          fundsAmount = {userInfo[0].accountBalance} />
+          
+          <div className='walletPage__details__contacts'> <ContactContainer /> </div>
+      </div>
     </div>
+  
   )
 }
 
