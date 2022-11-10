56b649e3c06a79aa69bb3e811c8f3d8a5d26d705
Renata Yakobov <rinat.yuri@gmail.com>
2022-11-09 22:24:22 -0500

    minor css updates

--- src/components/CurrencyConverter/CurrencyConverter.jsx
+++ src/components/CurrencyConverter/CurrencyConverter.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import "./CurrencyConverter.scss"
-import NavMenu from "../NavMenu/NavMenu"
-import LiveRates from '../LiveRates/LiveRates';
+
 
 const CurrencyConverter = () => {
   return (
@@ -9,6 +8,11 @@ const CurrencyConverter = () => {
        <h4>Convert</h4>
        <h2>Currency Converter</h2>  
        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea dolorem nihil neque laudantium assumenda totam amet alias, sequi fuga officiis asperiores beatae provident iste. Quis ducimus nobis a officia earum.</p>   
+       <div className='currencyConverter__main'>
+          <div className='currencyConverter__main__walletTemp'>Wallet Placeholder</div>
+          <div className='currencyConverter__main__converter'>Converter</div>
+       </div>
+       
     </div>
     
   )
