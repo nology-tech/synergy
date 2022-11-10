33f730c477000780a33b894fff2b7ccef4b200b8
RFowlerDB <rachie.fowler@db.com>
2022-11-10 12:24:28 -0500

    styling changes

--- src/components/FundsBox/FundsBox.jsx
+++ src/components/FundsBox/FundsBox.jsx
@@ -4,8 +4,12 @@ const FundsBox = (props) => {
     const { fundsAmount, forConverter } = props;
   return (
     <div className='fundsBox'>
-        <h3>Your Fund</h3>
-        <
+      <h3>Your Fund</h3>
+      <div className='fundsBox__background'></div>
+      <div className='fundsBox__blue'></div>
+      <div className='fundsBox_round'></div>
+        
+        
     </div>
   )
 }
