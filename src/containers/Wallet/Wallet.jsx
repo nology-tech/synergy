58ef9dc9dd9798a843fec06b85ad47692d2d2694
shirinnizan <shirinnizan@gmail.com>
2022-11-10 11:09:47 -0500

    component created

---
+++ src/containers/Wallet/Wallet.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import FundsBox from '../../components/FundsBox/FundsBox'
+
+const Wallet = () => {
+  return (
+    <div>
+        <h1>Funds box will go here</h1>
+        <FundsBox />
+    </div>
+  )
+}
+
+export default Wallet
