e46c734ee59fc3117522d9380407288f86ba1172
Rupali Anjaria-Rami <rupalianjaria-rami@Rupalis-iMac.home>
2022-11-10 23:16:43 -0500

    Fixed bug in Button Component

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,11 @@ import BillingAddress from "./components/BillingAddress/BillingAddress";
 import LandingMain from "./containers/LandingMain/LandingMain";
 import ContactListPage from "./components/ContactListPage/ContactListPage";
 import LiveRates from "./components/LiveRates/LiveRates";
+import Button from "./components/Button/Button";
 
-// import Button from "./components/Button/Button"; // to be removed
-// import {SlRefresh} from 'react-icons/sl';  // to be removed
+import {SlRefresh} from 'react-icons/sl';
+import {IoExitOutline} from "react-icons/io5";
+
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -26,52 +28,58 @@ const App = () => {
     setSearchTerm(cleanInput);
   };
 
-
-  // const buttonImg = <SlRefresh />;  // to be removed
-  // const buttonText = "Convert";  // to be removed
-
+  // Button Icons
+  // const buttonConvert = <SlRefresh />; 
+  // const buttonSignOut = <IoExitOutline />; 
 
   return (
     <Router>
       <div>
-                    {/* ===================================================================================================== */}
-                              {/* <div className="button-test" text = "FOR BUTTON TESTING ONLY - TO BE REMOVED AFTER TESTING" >
-                                  <Button
-                                    buttonStyle={"button-light-blue"}
-                                    isDisabled={false}
-                                    buttonType={""}
-                                    buttonText={"Find out more"}
-                                  />
-
-                                  <Button buttonStyle={"button-blue"} 
-                                  isDisabled={false} buttonType={""} buttonText={buttonText} buttonImg={buttonImg}/>
-
-                                  <Button buttonStyle={"button-blue-disabled"} 
-                                  isDisabled={false} buttonType={"submit"} buttonText={`Continue ▶`} />
-
-                                  <Button
-                                    buttonStyle={"button-blue-white-border"}
-                                    isDisabled={false}
-                                    buttonType={""}
-                                    buttonText={"Edit"}
-                                  />
-
-                                  <Button
-                                    buttonStyle={"button-dashed"}
-                                    isDisabled={false}
-                                    buttonType={""}
-                                    buttonText={"Add Currency"}
-                                  />
-
-                                  <Button
-                                    buttonStyle={"button-white"}
-                                    isDisabled={false}
-                                    buttonType={""}
-                                    buttonText={"Manage"}
-                                  />
-                              </div> */}
-                    {/* ===================================================================================================== */}
-
+        {/* Button Test */}
+        {/* <div className="button-test">
+          <Button
+            buttonStyle={"btn button-transparent"}
+            buttonText={"Find Out More"}
+          />
+          <Button 
+            buttonStyle={"btn button-blue"}
+            buttonText={"Sign Up"} 
+          />
+          <Button 
+            buttonStyle={"btn button-blue"}
+            buttonText={"Sign Out"} 
+            buttonImg={buttonSignOut}
+          />
+          <Button 
+            buttonStyle={"btn button-blue"}
+            buttonText={"Convert"}
+            buttonImg={buttonConvert}
+          />
+          <Button 
+            buttonStyle={"btn button-blue"}
+            buttonType={"submit"} 
+            buttonText={`Continue ▶`} 
+          />
+          <Button 
+            buttonStyle={"btn button-blue-disabled"}
+            buttonType={"submit"} 
+            buttonText={`Continue ▶`} 
+          />
+          <Button
+            buttonStyle={"btn button-blue-white-border"}
+            buttonText={"Edit"}
+          />
+          <Button
+            buttonStyle={"btn button-dashed"}
+            isDisabled={false}
+            buttonText={"Add Currency"}
+          />
+          <Button
+            buttonStyle={"btn button-white"}
+            isDisabled={false}
+            buttonText={"Manage"}
+          />
+        </div> */}
 
         <Routes>
           <Route path="/" element={<LandingMain />} />
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,50 +1,21 @@
 import React from "react";
-
-// 1. Button for LandingMain with OnClick functionality on div
-{/* 
-<div onClick={handleClick}>
-      <Button buttonStyle={"button-light-blue"} isDisabled={false} buttonType={""}  buttonText={"Find out more"} />
-</div> 
-*/}
-// ===========================================================================================================================
-// 2. Button for Converter with onClick functionality on div NEED TO TEST IF ICON IS BROUGHT IN
-// let buttonText = "<i class=\"fa-solid fa-arrows-rotate\"></i> Convert"
-{/* 
-<div onClick={handleClick}>
-      <Button buttonStyle={"button-blue"} buttonInitialStyle={""} isDisabled={false} buttonType={""} buttonText={buttonText} />
-</div> 
-*/}
-// ===========================================================================================================================
-// 3. Button for footerNav with onClick functionality on div
-{/* 
-<div onClick={handleClick}>
-      <Button buttonStyle={"button-blue"} buttonInitialStyle={""} isDisabled={false} buttonType={""} buttonText={"Sign-Up"} />
-</div> 
-*/}
-// ===========================================================================================================================
-// 4. Button for LiveRates with onClick functionality on div
-{/*
-    <Button buttonStyle={"button-blue"} buttonInitialStyle={"white-border"} isDisabled={false} buttonType={""} buttonText={"Edit"} />
-*/}
-// ===========================================================================================================================
-// 5. Button for CreateAccount with type submit NEED TO TEST ON VALIDATION BUTTON IS ENABLED
-{/*
-    <Button buttonStyle={"button-blue"} buttonInitialStyle={"disabled"} isDisabled={!(validEmail && validPassword && validConfirmedPassword)} buttonType={"submit"} buttonText={"Continue &#9654"} />
-*/}
-// ===========================================================================================================================
+import "./Button.scss";
 
 const Button = (props) => {
 
-  const { buttonImg, buttonStyle, isDisabled, buttonType, buttonText} = props;
-
-  return 
+  const { buttonStyle, isDisabled, onClick, buttonType, buttonImg, buttonText } = props;
+  return (
+    <>
     <button 
-      className={buttonStyle} 
-      type={buttonType} 
-      disabled={isDisabled}
-    > 
-      {buttonImg}   {buttonText} 
-    </button>;
-  };
+      className = {buttonStyle} 
+      type = {buttonType} 
+      onClick = {onClick}
+      disabled = {isDisabled}> 
+      {buttonText} {buttonImg}
+    </button>
+    </>
+  )
+}
 
-  export default Button;
+export default Button
+
