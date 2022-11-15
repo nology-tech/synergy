30144e444126492d3d35ea649172ce89671a5cd7
shirinnizan <shirinnizan@gmail.com>
2022-11-15 16:55:46 -0500

    more styling added on the fund amount

--- src/components/FundsBox/FundsBox.jsx
+++ src/components/FundsBox/FundsBox.jsx
@@ -7,21 +7,26 @@ const FundsBox = (props) => {
   const { fundsAmount, forConverter, name } = props;
 
   return (
-    <div className="fundsBox-wrapper">
-    <h2 className="greeting__firstName">{"Hey " + name}
-    <img className="greetings__wave" src={highFive} alt="highFiveWave"/>
-    </h2>
+    <>
+      <h2 className="greeting__firstName">
+        {"Hey " + name}
+        <img className="greetings__wave" src={highFive} alt="highFiveWave" />
+      </h2>
       <div className="fundsBox">
         <div className="fundsBox__content">
-          <img className="fundsBox_square" src={blueSquare} alt="blueSquareBox"/>
-            <h3 className="fundsBox__content__title">Your Funds:</h3>
-            <div className="fundsBox__content__amount">{"\u00A3" + fundsAmount.toLocaleString("en-US")}</div>
-            <button className="fundsBox__content__manage"> Manage </button>
+          <img
+            className="fundsBox_square"
+            src={blueSquare}
+            alt="blueSquareBox"
+          />
+          <h3 className="fundsBox__content__title">Your Funds:</h3>
+          <div className="fundsBox__content__amount">
+            {"\u00A3" + fundsAmount.toLocaleString("en-US")}
+          </div>
+          <button className="fundsBox__content__manage"> Manage </button>
         </div>
-        
       </div>
-      
-    </div>
+    </>
   );
 };
 
