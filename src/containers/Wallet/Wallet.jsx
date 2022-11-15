7350370ba27e5fbce6e29eb6015eb1c895605c09
shirinnizan <shirinnizan@gmail.com>
2022-11-15 15:08:05 -0500

    navmenu added

--- src/containers/Wallet/Wallet.jsx
+++ src/containers/Wallet/Wallet.jsx
@@ -4,26 +4,29 @@ import userInfo from '../../data/userInfo.js';
 import NavMenu from '../../components/NavMenu/NavMenu';
 import ContactContainer from '../ContactContainer/ContactContainer';
 import NavBar from "../../assets/images/navbar.png";
+import DashboardHeader from '../../components/DashboardHeader/DashboardHeader';
 
 const Wallet = () => {
   return (
-    <div className='walletPage'>
-      <div className='walletPage__placeHolder'>
-          <div className="walletPage__placeHolder__Navbar"> <img src={NavBar} alt="NavBar" /> </div>
-          <div className="walletPage__placeHolder__NavMenu"> <NavMenu /> </div>     
-      </div>    
-      <div className='walletPage__details'>
+    <div className="wallet-page">
+      <div className="wallet-page__menu">
+        <NavMenu />
+      </div>
+      <div className="wallet-page__header">
+        <DashboardHeader />
+      </div>
+      {/* <div className='walletPage__details'>
           <h1 className='walletPage__details__title'>Funds box will go here</h1>
           {/* {console.log(userInfo[0].accountBalance)}; */}
-          <FundsBox 
-          name = {userInfo[0].firstName}
-          fundsAmount = {userInfo[0].accountBalance} />
-          
-          <div className='walletPage__details__contacts'> <ContactContainer /> </div>
+      <div className="wallet-page__main">
+        <FundsBox
+          name={userInfo[0].firstName}
+          fundsAmount={userInfo[0].accountBalance}
+        />
       </div>
+      <div className='wallet-page__contacts'> <ContactContainer /></div>
     </div>
-  
-  )
+  );
 }
 
 export default Wallet
