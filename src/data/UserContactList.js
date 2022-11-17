3652b87cc9fd07081b2ecc83d629286c3ff1dcd1
Eva S <evamarie.songco@gmail.com>
2022-11-17 12:13:58 -0800

    Updated the userContacts.js data file as agreed with Pavi

--- src/data/UserContactList.js
+++ src/data/UserContactList.js
@@ -1,53 +1,24 @@
 const UserContactList = [
   {
     uid: 1234560,
-    account: 13264514864,
-  },
-  {
-    uid: 1234560,
-    account: 13264514867,
-  },
-  {
-    uid: 1234560,
-    account: 13264514869,
+    // account: 13264514864,
+    contacts:{contactId:13264514864, contactId: 13264514867, account: 13264514869,account: 13264584897}
   },
   {
     uid: 1234561,
-    account: 13264514868,
+    contacts: {account: 13264514894}
   },
   {
-    uid: 1234561,
-    account: 13464514862,
-  },
-
-  {
-    uid: 1234563,
-    account: 13264514874,
+    uid: 1234562,
+    contacts:{contactId:13264514868, account: 13464514862}
   },
   {
     uid: 1234563,
-    account: 13264614867,
-  },
-  {
-    uid: 1234563,
-    account: 13964514869,
+    contacts:{account: 13264514874,account: 13264614867,account: 13964514869,}
   },
   {
     uid: 1234564,
-    account: 13264584868,
-  },
-  {
-    uid: 1234564,
-    account: 13264514818,
-  },
-
-  {
-    uid: 1234560,
-    account: 13264584897,
-  },
-  {
-    uid: 1234560,
-    account: 13264514894,
+    contacts:{account: 13264584868,account: 13264514818}
   },
 
 ];
