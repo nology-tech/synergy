30e7970701ff328cc64623c44ef04b323a16a1d9
Eva S <evamarie.songco@gmail.com>
2022-11-17 19:33:21 -0800

    Updated UserContactList data structure

--- src/data/UserContactList.js
+++ src/data/UserContactList.js
@@ -1,26 +1,26 @@
 const UserContactList = [
   {
     uid: 1234560,
-    contacts:{account:13264514864, account: 13264514867, account: 13264514869,account: 13264584897}
+    contacts:[13264514864, 13264514867, 13264514869,13264584897]
   },
   {
     uid: 1234561,
-    contacts: {account: 13264514894}
+    contacts: [13264514894]
   },
   {
     uid: 1234562,
-    contacts:{account:13264514868, account: 13464514862}
+    contacts:[13264514868,13464514862]
   },
   {
     uid: 1234563,
-    contacts:{account: 13264514874,account: 13264614867,account: 13964514869,}
+    contacts:[13264514874,13264614867,13964514869]
   },
   {
     uid: 1234564,
-    contacts:{account: 13264584868,account: 13264514818}
+    contacts:[13264584868,13264514818]
   },
   {uid: 1234567,
-  contacts:{}
+  contacts:[]
   }
 
 ];
