18ef9c0741d247b7630c20c6379d3e8f2b89c515
Tet <Maria-Theresa.Edralin@db.com>
2022-11-23 10:34:18 -0500

    removed commented lines in liverates

--- src/components/LiveRates/LiveRates.jsx
+++ src/components/LiveRates/LiveRates.jsx
@@ -21,20 +21,7 @@ const LiveRates = () => {
         </div>
         <div className="live-rates">
           <h4 className="live-rates__h4">Transfer</h4>
-          {/* <h2 className="live-rates__h2">View Latest Rates</h2>
-          <p className="live-rates__para">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam,
-            voluptatem quidem? Corporis, odio vero sequi vitae deleniti sit quod
-            quae! Autem unde reiciendis sunt porro obcaecati alias fugiat quod
-            aliquid!
-          </p> */}
           <LiveRatesList className="live-rates__list" currency={currency} />
-          {/* <Button
-            buttonStyle={"btn button-dashed  button secondary"}
-            isDisabled={false}
-            buttonText={"Add Currency"}
-          /> */}
-          {/* <button className="button secondary">Add Currency</button> */}
         </div>
       </div>
     </div>
