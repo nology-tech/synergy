6bc23938c2e6a787172f73588878d46c7fc32dc5
Sheena Abraham <SheenaPAbraham@gmail.com>
2022-11-30 15:05:14 -0500

    Fixed clickable space in row and made delete purely delete only

--- src/components/ContactRow/ContactRow.jsx
+++ src/components/ContactRow/ContactRow.jsx
@@ -4,21 +4,22 @@ import "./ContactRow.scss";
 const ContactRow = ({contact, onContactClick, onDelete}) => {
    // Table of contacts tr = table row; td = table details
     return (
-        <tr className="contactRow" key={contact.account}  
-        onClick={() => onContactClick(contact.account)}
-        >
-            <td className="contactRow__bankIcon">
+        <tr className="contactRow" key={contact.account}>
+        {/* <div className="contactRow__trcontent"> */}
+            <td className="contactRow__bankIcon" onClick={() => onContactClick(contact.account)}>
                 <img
                 className="contactRow__bankIcon__img"
                 src={contact.bankIcon}
                 alt={contact.name}
                 />
             </td>
-            <td className="contact__name">{contact.firstName + " " + contact.lastName}</td>
-            <td className="contact__sortCode">{contact.sortCode}</td>
-            <td className="contact__account">{contact.account}</td>
-            <td className="contact__bankName">{contact.bankName}</td>
-            <td className="contact__iban">{contact.IBAN}</td>
+            <td className="contact__name" onClick={() => onContactClick(contact.account)}>{contact.firstName + " " + contact.lastName}</td>
+            <td className="contact__sortCode" onClick={() => onContactClick(contact.account)}>{contact.sortCode}</td>
+            <td className="contact__account" onClick={() => onContactClick(contact.account)}>{contact.account}</td>
+            <td className="contact__bankName" onClick={() => onContactClick(contact.account)}>{contact.bankName}</td>
+            <td className="contact__iban" onClick={() => onContactClick(contact.account)}>{contact.IBAN}</td>
+        {/* </div> */}
+        {/* <div className="contactRow__delete"> */}
             <td className="contact__delete">
                 <img
                 className="contactRow__delete__icon"
@@ -27,6 +28,7 @@ const ContactRow = ({contact, onContactClick, onDelete}) => {
                 onClick={() => onDelete(contact.account)}
                 />
             </td>
+        {/* </div> */}
         </tr>
   );
 };
