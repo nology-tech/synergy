ac1637d495547985e486584c67d9ffea21ffc513
Tina Samuels <mssamuelstina@gmail.com>
2022-12-07 18:06:02 -0500

    change the bank information in ContactAdd

--- src/components/ContactAdd/ContactAdd.jsx
+++ src/components/ContactAdd/ContactAdd.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./ContactAdd.scss";
 import { useState } from "react";
+import { useEffect } from "react";
 import TransferAddRecipient from "../TransferAddRecipient/TransferAddRecipient";
 import TransferConfirmRecipient from "../TransferConfirmRecipient/TransferConfirmRecipient";
 import ContactListPage from "../ContactListPage/ContactListPage";
@@ -52,10 +53,39 @@ const ContactAdd = () => {
     };
 
     //Add Recepient
-  const [recipientName, setRecipientName] = useState("");
-  const [accountTypeRecipient, setAccountTypeRecipient] = useState();
-  const [accountNumRecipient, setAccountNumRecipient] = useState();
-  const [sortCodeRecipient, setSortCodeRecipient] = useState("");
+    const [recipientName, setRecipientName] = useState("");
+    const [accountTypeRecipient, setAccountTypeRecipient] = useState();
+    const [accountNumRecipient, setAccountNumRecipient] = useState();
+    const [currencyRecipient, setCurrencyRecipient] = useState();
+    const [bankRecipient, setBankRecipient] = useState();
+    const [sortCodeRecipient, setSortCodeRecipient] = useState("");
+
+    const [banks, setBanks] = useState(
+      [
+      {
+        bankName: "",
+        bankLogo: "",
+      },
+    ]
+    );
+  
+    const getBanks = () => {
+      
+      //e.preventDefault();
+      fetch("http://localhost:8080/banks", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+       
+      })
+        .then((response) => response.json())
+        .then((json) => setBanks(json))
+        .catch((err) => console.log(err));
+      //e.target.reset();
+    };
+  
+   useEffect (() => getBanks(),[]);
 
   // Handling the recipeint name input field change
   const handleRecipientName = (e) => {
@@ -81,6 +111,18 @@ const ContactAdd = () => {
     setSortCodeRecipient(e.target.value.toString());
   };
 
+   // Handling the account currency input field change
+   const handleCurrencyRecipient = (e) => {
+    e.preventDefault();
+    setCurrencyRecipient(e.target.value);
+  };
+
+   // Handling the bank input field change
+   const handleBankRecipient = (e) => {
+    e.preventDefault();
+    setBankRecipient(e.target.value);
+  };
+
   //function to display different stages of adding contact
   const displayCurrentView = () => {
     if (workflowStage === "contactContainer") {
@@ -90,6 +132,15 @@ const ContactAdd = () => {
         <>
           <ContactListPage handleAddContact={handleAddContact} />
           <TransferAddRecipient
+          recipientName={recipientName}
+          accountTypeRecipient={accountTypeRecipient}
+          accountNumRecipient={accountNumRecipient}
+          sortCodeRecipient={sortCodeRecipient}
+          currencyRecipient={currencyRecipient}
+          bankRecipient={bankRecipient}
+          banks={banks}
+          handleCurrencyRecipient={handleCurrencyRecipient}
+          handleBankRecipient={handleBankRecipient}         
             handleContinueButton={handleContinueButton}
             handleGoBack={handleGoBack}
             handleRecipientName={handleRecipientName}
@@ -97,10 +148,10 @@ const ContactAdd = () => {
             handleAccountNumRecipient={handleAccountNumRecipient}
             handleSortCodeRecipient={handleSortCodeRecipient}
             handleCloseWindow={handleCloseWindow}
-            recipientName={recipientName}
-            accountTypeRecipient={accountTypeRecipient}
-            accountNumRecipient={accountNumRecipient}
-            sortCodeRecipient={sortCodeRecipient}
+            
+            
+            
+            
           />
         </>
       );
