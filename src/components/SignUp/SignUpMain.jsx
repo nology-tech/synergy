209e8b8571f3ce7181be72ae39056392427dbf9b
ppatilgit <113374717+ppatilgit@users.noreply.github.com>
2022-12-07 22:19:50 -0500

    Update SignUpMain.jsx

--- src/components/SignUp/SignUpMain.jsx
+++ src/components/SignUp/SignUpMain.jsx
@@ -2,15 +2,21 @@ import CreateAccount from "../../containers/CreateAccount/CreateAccount";
 import SignInNav from "../SignInNav/SignInNav";
 // import SignUp from "../../components/SignUp/SignUp";
 
-const SignUpMain = () => {
+const SignUpMain = (props) => {
+  const {email, handleEmail, validEmail}=props;
   return (
     // <div className="signup-main-container">
     <div className="signin__page">
       <SignInNav title="Login" />
-      <div className="signupmain__container"><CreateAccount /></div>
+      <div className="signupmain__container"><CreateAccount 
+                email={email}
+                handleEmail={handleEmail}
+                validEmail={validEmail}
+        /></div>
     </div>
     // </div>
   );
 };
 
 export default SignUpMain;
+
