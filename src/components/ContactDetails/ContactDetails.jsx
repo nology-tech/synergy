43092e55adf171b288981cc8822ef6a992648419
Rupali Anjaria-Rami <rupalianjaria-rami@Rupalis-iMac.home>
2022-12-14 13:08:05 -0500

    formatting contact-transaction details started

--- src/components/ContactDetails/ContactDetails.jsx
+++ src/components/ContactDetails/ContactDetails.jsx
@@ -20,13 +20,13 @@ const ContactDetails = (props) => {
   const accountIBANRecipient = "GB10HLFX1100011000005";
 
   return (
-    <div className="contact-details">
-      <div className="contact-details__header">
+    <div className="contact-details-transactions">
+      <div className="contact-details-transactions__header">
         <h2>{"Contacts / " + recipientName}</h2>
         <h1>Account Details</h1>
       </div>
-      <div className="transaction-details_main">
-        <div className="contact-details__main">
+      <div className="contact-details-transactions__main">
+        <div className="contact-details">
           <div className="contact-details__account-recipient">
             <h3 className="contact-details__account-recipient__recusername">
               {recipientName}
@@ -57,6 +57,44 @@ const ContactDetails = (props) => {
       </div>
     </div>
   );
+  // return (
+  //   <div className="contact-details">
+  //     <div className="contact-details__header">
+  //       <h2>{"Contacts / " + recipientName}</h2>
+  //       <h1>Account Details</h1>
+  //     </div>
+  //     <div className="transaction-details_main">
+  //       <div className="contact-details__main">
+  //         <div className="contact-details__account-recipient">
+  //           <h3 className="contact-details__account-recipient__recusername">
+  //             {recipientName}
+  //           </h3>
+  //           <div className="contact-details__account-recipient__details">
+  //             <h3>Sort Code:</h3>
+  //             <p>{sortCodeRecipient}</p>
+  //           </div>
+  //           <div className="contact-details__account-recipient__details">
+  //             <h3> Account Number: </h3>
+  //             <p>{accountNumRecipient}</p>
+  //           </div>
+  //           <div className="contact-details__account-recipient__details">
+  //             <h3> IBAN: </h3>
+  //             <p>{accountIBANRecipient}</p>
+  //           </div>
+  //         </div>
+  //         <div className="contact-details__buttons">
+  //           <h2>{"Want to make transfer?"}</h2>
+  //           <Link to="/currencyconverter">
+  //             <Button buttonStyle={"btn button-blue"} buttonText={"Transfer"} />
+  //           </Link>
+  //         </div>
+  //       </div>
+  //       <div className="transaction-details">
+  //         <TransactionList transactionsArray={transactionsArray} />
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 };
 
 export default ContactDetails;
